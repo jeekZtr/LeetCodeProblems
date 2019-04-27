74352e45233cf1104c621917d4981a5d06db1ec6
zhangtairan <zhangtr1116@163.com>
2019-04-27 22:22:55 +0800

    feat:

--- src/com/leetcode/explore/primaryalgorithm/String9.java
+++ src/com/leetcode/explore/primaryalgorithm/String9.java
@@ -1,7 +1,12 @@
 package com.leetcode.explore.primaryalgorithm;
 
+import java.util.Arrays;
+
 /**
- *  编写一个函数来查找字符串数组中的最长公共前缀。
+ *  
+ *  
+ *  
+      编写一个函数来查找字符串数组中的最长公共前缀。
 	如果不存在公共前缀，返回空字符串 ""。
 	示例 1:
 	输入: ["flower","flow","flight"]
@@ -16,24 +21,33 @@ package com.leetcode.explore.primaryalgorithm;
  */
 public class String9 {
 	public static void main(String args[]) {
+		String[] strs = {};
+		System.out.println(longestCommonPrefix(strs));
 	}
 
 	 public static String longestCommonPrefix(String[] strs) {
 		 if(strs == null  ) {
 			 return "";
 		 }
-		 if(strs.length == 1 ||  "".equals(strs[0] )) {
-			 return strs[0];
+		 if(  strs.length == 0 ||  strs==null ||  "".equals(strs[0])  ) {
+			 return "";
 		 }
+		 String str = strs[0];
 		 
-		 for(int j=1;j<strs.length;j++) {
-			 String str = strs[j];
-			 for(int i=0;i< str.length()- strs[0].length()+1;i++) {
-					String temp = str.substring(i,i+ strs[0].length());
-					if(strs[0].equals(temp )) {
+		 for(int i=0;i< str.length() ;i++) {
+			 for(int j=1; j<strs.length; j++ ) {
+				 	String str1 = strs[j];
+					char temp = str.charAt(i);
+					if(i>=str1.length()) {
+						return str.substring(0, i);
+					}
+					char temp1 = str1.charAt(i);
+					if(temp == temp1 ) {
+					}else {
+						return str.substring(0, i);
 					}
 			}
 		 }
-	     return "";
+	     return str;
 	}
 }
